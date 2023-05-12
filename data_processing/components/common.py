708d3d088a38a9239714ea6207f9b48fa5a4dbd7
Stefan Krawczyk <stefan@dagworks.io>
2023-05-12 11:03:29 -0700

    Data processing - commit 2

--- data_processing/components/common.py
+++ data_processing/components/common.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from hamilton.function_modifiers import save_to, source
 
 def orders_denormalized(orders: pd.DataFrame, order_details: pd.DataFrame) -> pd.DataFrame:
     _df = pd.merge(orders, order_details, how="inner", on=["order_id", "product_name"])
@@ -7,6 +7,7 @@ def orders_denormalized(orders: pd.DataFrame, order_details: pd.DataFrame) -> pd
     return _df
 
 
+@save_to.csv(path=source('output_path'))
 def orders_by_order_aggregates(orders_denormalized: pd.DataFrame) -> pd.DataFrame:
     _df = orders_denormalized.groupby(["order_id", "customer_name", "order_date"]).agg(
         {"item_total": "sum", "quantity": "sum"}
